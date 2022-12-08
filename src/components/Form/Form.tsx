626d6a96feb7e368aefa0ab46087d1c8213a84f6
Luis Dudamel <ladusa@gmail.com>
2022-12-08 16:33:07 +0100

    Added first input group of protected fields

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,12 +1,58 @@
-import { FormContainerStyled, FormInformationStyled } from "./FormStyled";
+import {
+  FormContainerStyled,
+  FormInformationStyled,
+  FormStyled,
+} from "./FormStyled";
 
 const Form = (): JSX.Element => {
   return (
     <FormContainerStyled>
       <FormInformationStyled>
         Please type carefully and fill out the form with your personal details.
-        Your can’t edit these details once you submitted the form.
+        You cannot edit these details once you submitted the form.
       </FormInformationStyled>
+      <FormStyled>
+        <label htmlFor="first name" className="form__input-label">
+          First Name
+        </label>
+        <input
+          type="text"
+          id="first name"
+          className="form__input-container"
+          placeholder="Info from registration form"
+          disabled
+        />
+        <label htmlFor="last name" className="form__input-label">
+          Last Name
+        </label>
+        <input
+          type="text"
+          id="last name"
+          className="form__input-container"
+          placeholder="Info from registration form"
+          disabled
+        />
+        <label htmlFor="email address" className="form__input-label">
+          Email Address
+        </label>
+        <input
+          type="text"
+          id="first name"
+          className="form__input-container"
+          placeholder="Info from registration form"
+          disabled
+        />
+        <label htmlFor="phone number" className="form__input-label">
+          Phone Number
+        </label>
+        <input
+          type="text"
+          id="phone number"
+          className="form__input-container"
+          placeholder="Info from registration form"
+          disabled
+        />
+      </FormStyled>
     </FormContainerStyled>
   );
 };
--- src/components/Form/FormStyled.ts
+++ src/components/Form/FormStyled.ts
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 export const FormContainerStyled = styled.div`
-  border: solid 1px red;
   display: flex;
   height: 100%;
   width: 100%;
   align-items: flex-start;
-  padding: 30px 0px;
+  flex-direction: column;
+  padding: 25px 0px;
 `;
 
 export const FormInformationStyled = styled.p`
@@ -21,4 +21,44 @@ export const FormInformationStyled = styled.p`
   letter-spacing: 0.05em;
   color: black;
   padding: 0px 10px;
+`;
+
+export const FormStyled = styled.form`
+  width: 100%;
+
+  padding: 50px 15px;
+
+  .form__input {
+    &-label {
+      font-family: "Sansation Light";
+      font-style: normal;
+      font-weight: 300;
+      font-size: 14px;
+      line-height: 20px;
+      display: flex;
+      align-items: center;
+      letter-spacing: 0.05em;
+      padding-bottom: 5px;
+    }
+
+    &-container {
+      width: 100%;
+      height: 30px;
+      background: rgba(230, 230, 230, 0.35);
+      border: 1px solid;
+      border-color: ${({ theme }) => theme.colors.mainColor};
+      border-radius: 5px;
+      font-family: "Sansation Light";
+      font-style: normal;
+      font-weight: 300;
+      font-size: 14px;
+      line-height: 20px;
+      display: flex;
+      align-items: center;
+      letter-spacing: 0.05em;
+      color: #949494;
+      padding-left: 10px;
+      margin-bottom: 20px;
+    }
+  }
 `;
