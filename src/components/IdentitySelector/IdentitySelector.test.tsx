b73748a5128cbb94a60f586221930d1212c7003f
Luis Dudamel <ladusa@gmail.com>
2022-12-09 10:23:32 +0100

    Added test for enabling next step button

--- src/components/IdentitySelector/IdentitySelector.test.tsx
+++ src/components/IdentitySelector/IdentitySelector.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 import IdentitySelector from "./IdentitySelector";
 import { light } from "../../styles/Theme.styled";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a IdentitySelector function", () => {
   describe("When it's invoked", () => {
@@ -29,6 +30,28 @@ describe("Given a IdentitySelector function", () => {
       expect(passportButton).toBeInTheDocument();
       expect(nationalCardButton).toBeInTheDocument();
       expect(driversLicense).toBeInTheDocument();
+    });
+  });
+  describe("When it's invoked and the user clicks on the Passport button", () => {
+    test("Then the button with the text `NEXT STEP` should be enabled", async () => {
+      const expectedPassportText = "Id Icon Passport";
+      const expectedNextButtonText = "NEXT STEP";
+
+      render(
+        <ThemeProvider theme={light}>
+          <IdentitySelector />
+        </ThemeProvider>
+      );
+
+      const passportButton = screen.getByRole("button", {
+        name: expectedPassportText,
+      });
+      const nextButton = screen.getByRole("button", {
+        name: expectedNextButtonText,
+      });
+      await userEvent.click(passportButton);
+
+      expect(nextButton).toBeEnabled();
     });
   });
 });
