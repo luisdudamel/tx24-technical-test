2df609eb38bd2476a19c5f0fc2dddca8c909083e
Luis Dudamel <ladusa@gmail.com>
2022-12-11 14:24:49 +0100

    Added footer info list and logo for desktop

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,9 @@
 import {
   DesktopFooter,
   FooterCopyright,
+  FooterInfo,
   FooterInfoContainer,
+  FooterLogo,
   FooterStyled,
 } from "./FooterStyled";
 
@@ -12,7 +14,29 @@ const Footer = (): JSX.Element => {
         Copyright © 2022 FXNET - FZE. All Rights Reserved.
       </FooterStyled>
       <DesktopFooter>
-        <FooterInfoContainer></FooterInfoContainer>
+        <FooterInfoContainer>
+          <FooterLogo src="img/logo-tx24.svg" alt="TX-24 Logo" />
+          <FooterInfo>
+            <div className="footer__info__panel">
+              <p className="footer__info__list__title">Company</p>
+              <ul>
+                <li className="footer__info__list__item">Terms & Service</li>
+                <li className="footer__info__list__item">Privacy Policy</li>
+                <li className="footer__info__list__item">About</li>
+                <li className="footer__info__list__item">Careers</li>
+              </ul>
+            </div>
+            <div className="footer__info__panel">
+              <p className="footer__info__list__title">About Us</p>
+              <ul>
+                <li className="footer__info__list__item">The Team</li>
+                <li className="footer__info__list__item">Whitepaper</li>
+                <li className="footer__info__list__item">Blog</li>
+                <li className="footer__info__list__item">Contact</li>
+              </ul>
+            </div>
+          </FooterInfo>
+        </FooterInfoContainer>
         <FooterCopyright>
           Copyright © 2022 FXNET - FZE. All Rights Reserved.
         </FooterCopyright>
--- src/components/Footer/FooterStyled.tsx
+++ src/components/Footer/FooterStyled.tsx
@@ -22,10 +22,11 @@ export const FooterStyled = styled.footer`
 
 export const DesktopFooter = styled.footer`
   display: flex;
+  flex-direction: column;
   margin-top: 30px;
   width: 100%;
   background: #fff4f9;
-  height: 215px;
+  height: 230px;
   text-align: center;
   padding-bottom: 0px;
   padding-top: 0px;
@@ -41,16 +42,60 @@ export const DesktopFooter = styled.footer`
   }
 `;
 
-export const FooterInfoContainer = styled.div``;
+export const FooterInfoContainer = styled.div`
+  display: flex;
+  height: 100%;
+  padding-left: 50px;
+  align-items: center;
+`;
 
-export const FooterInfo = styled.div``;
+export const FooterInfo = styled.div`
+  display: flex;
+  width: 100%;
+  padding-left: 100px;
 
-export const FooterLogo = styled.img``;
+  ul {
+    list-style: none;
+    text-align: left;
+    padding-inline-start: 0px;
+  }
+
+  .footer__info {
+    &__panel {
+      padding: 0px 20px;
+    }
+    &__list {
+      &__title {
+        font-family: "Sansation Light";
+        font-style: normal;
+        font-weight: 300;
+        font-size: 20px;
+        line-height: 30px;
+        letter-spacing: 0.035em;
+        color: #000000;
+        text-align: left;
+      }
+      &__item {
+        font-family: "Sansation Light";
+        font-style: normal;
+        font-weight: 300;
+        font-size: 16px;
+        line-height: 25px;
+        letter-spacing: 0.035em;
+        color: #000000;
+      }
+    }
+  }
+`;
+
+export const FooterLogo = styled.img`
+  width: 150px;
+`;
 
 export const FooterCopyright = styled.div`
-  margin-top: 30px;
   width: 100%;
   height: 30px;
+  background: #fff4f9;
   text-align: center;
   padding-bottom: 0px;
   padding-top: 0px;
