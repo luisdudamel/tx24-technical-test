d83f214cd2c978a83f9d3a8d0997d2167f1c293d
Luis Dudamel <ladusa@gmail.com>
2022-12-11 13:58:11 +0100

    Added footer container for desktop breakpoint

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,23 @@
-import { FooterStyled } from "./FooterStyled";
+import {
+  DesktopFooter,
+  FooterCopyright,
+  FooterInfoContainer,
+  FooterStyled,
+} from "./FooterStyled";
 
 const Footer = (): JSX.Element => {
   return (
-    <FooterStyled>
-      Copyright © 2022 FXNET - FZE. All Rights Reserved.
-    </FooterStyled>
+    <>
+      <FooterStyled>
+        Copyright © 2022 FXNET - FZE. All Rights Reserved.
+      </FooterStyled>
+      <DesktopFooter>
+        <FooterInfoContainer></FooterInfoContainer>
+        <FooterCopyright>
+          Copyright © 2022 FXNET - FZE. All Rights Reserved.
+        </FooterCopyright>
+      </DesktopFooter>
+    </>
   );
 };
 
--- src/components/Footer/FooterStyled.tsx
+++ src/components/Footer/FooterStyled.tsx
@@ -14,4 +14,50 @@ export const FooterStyled = styled.footer`
   line-height: 30px;
   letter-spacing: 0.035em;
   color: #000000;
+
+  @media (min-width: 769px) {
+    display: none;
+  }
+`;
+
+export const DesktopFooter = styled.footer`
+  display: flex;
+  margin-top: 30px;
+  width: 100%;
+  background: #fff4f9;
+  height: 215px;
+  text-align: center;
+  padding-bottom: 0px;
+  padding-top: 0px;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 12px;
+  line-height: 30px;
+  letter-spacing: 0.035em;
+  color: #000000;
+
+  @media (max-width: 769px) {
+    display: none;
+  }
+`;
+
+export const FooterInfoContainer = styled.div``;
+
+export const FooterInfo = styled.div``;
+
+export const FooterLogo = styled.img``;
+
+export const FooterCopyright = styled.div`
+  margin-top: 30px;
+  width: 100%;
+  height: 30px;
+  text-align: center;
+  padding-bottom: 0px;
+  padding-top: 0px;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 12px;
+  line-height: 30px;
+  letter-spacing: 0.035em;
+  color: #000000;
 `;
